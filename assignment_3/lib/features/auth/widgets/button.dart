3e8ffb1a434409ee92ac4cf5386d8ff2a621fa89
chacha <86880916+hansejun@users.noreply.github.com>
2025-01-23 02:27:11 +0900

    Update button.dart

--- assignment_3/lib/features/auth/widgets/button.dart
+++ assignment_3/lib/features/auth/widgets/button.dart
@@ -4,18 +4,25 @@ import 'package:flutter/material.dart';
 class Button extends StatelessWidget {
   final String text;
   final VoidCallback onTap;
-  const Button({super.key, required this.text, required this.onTap});
+  final Color? color;
+  final bool isDisabled;
+  const Button(
+      {super.key,
+      required this.text,
+      required this.onTap,
+      this.color,
+      this.isDisabled = false});
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
-      onTap: onTap,
+      onTap: isDisabled ? null : onTap,
       child: Container(
         alignment: Alignment.center,
         height: Sizes.size56,
         width: double.infinity,
         decoration: BoxDecoration(
-          color: Colors.grey.shade900,
+          color: color ?? Colors.grey.shade900,
           borderRadius: BorderRadius.circular(100),
         ),
         child: Text(text,
